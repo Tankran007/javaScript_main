44fbdcec0fba1ee91c556dc17d07061eea3bcf29
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-01-26 16:01:37 +0700

    update

---
+++ test/test001.js
@@ -0,0 +1,8 @@
+function numberSplit(num1){
+    let half = num1 /2;
+    return [Math.floor(half),Math.ceil(half)]
+}
+console.log(numberSplit(4));
+console.log(numberSplit(10));
+console.log(numberSplit(11));
+console.log(numberSplit(-9));
